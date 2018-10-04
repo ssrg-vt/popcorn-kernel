cc694bd7085fa8f48db6e5f0bc2c7bd61f85903a
Ho-Ren(Jack) Chuang <horenc@vt.edu>
2018-10-03 23:19:45 -0400

    multi-writer: barrier + scatter msgs

--- kernel/popcorn/sync.c
+++ kernel/popcorn/sync.c
@@ -21,6 +21,8 @@
 #include "trace_events.h"
 #include <linux/delay.h>
 
+#define POPCORN_BARRIER 1
+#define INVOKE_INV 0
 #define REENTRY_BEGIN_DISABLE 1 // 0: repeat show 1: once
 bool batch = true;
 //bool batch = false;
@@ -36,36 +38,100 @@ spinlock_t tso_lock;
 
 static bool print = false;
 
+/* ugly */
 extern void __print_pids(struct remote_context *rc);
-
-/* Local -> */
-void __find_conflictions(int nid)
+extern int sync_server_local_conflictions(struct remote_context *rc);
+/* (serial/leader)[Local] -> */
+void __find_conflictions(int nid, struct remote_context *rc)
 {
     page_merge_request_t *req = pcn_kmsg_get(sizeof(*req));
 	struct wait_station *ws = get_wait_station(current);
-	struct remote_context *rc = get_task_remote(current);
+	int i;
+	int iter = 0;
+	int sent_cnt = 0;
+	int single_sent = 0;
+	int total_iter;
+	bool zero_case = true;
+	int local_wr_cnt;
 
 	req->origin_pid = my_nid;
     req->origin_ws = ws->id;
     req->remote_pid = rc->remote_tgids[nid];
 
-    //req->tso_wr_cnt =
-    //req->addrs[MAX_WRITE_INV_BUFFERS] =
-	/* TODO:
+	/* TODO: optimizations
 	 * 1. construct req->addrs[]
 	 *		1.1 hastable local addrs
 	 */
-	sync_server_local_conflictions(rc);
-	/* read from garry */
-	//rc->addrs[rc->addr_cnt];
-	printk("addr_cnt %d\n", rc->addr_cnt);
-												BUG_ON(rc->addr_cnt);
+	local_wr_cnt = sync_server_local_conflictions(rc);
 
+	// move result to the msg
+	// optmization: remove this copy by moving into the func()
+	/* dbg */
+	//rc->inv_addrs[rc->lconf_cnt];
+	if (rc->lconf_cnt)
+		PCNPRINTK_ERR("local_conflict_addr_cnt %d\n", rc->lconf_cnt);
+	BUG_ON(rc->lconf_cnt > MAX_ALIVE_THREADS * MAX_WRITE_INV_BUFFERS);
+
+	/* scatters - send */
+	/* even send when 0 */
+	if (local_wr_cnt == 0) {
+		iter = 0; /* total == 0 iter == 0 */
+		total_iter = 0;
+	} else {
+		iter++; /* total > 0 iter start from 1 */
+		total_iter = (local_wr_cnt / MAX_WRITE_INV_BUFFERS) + 1;
+		zero_case = false;
+	}
+
+	do {
+		single_sent = local_wr_cnt - sent_cnt;
+		if (single_sent > (int)MAX_WRITE_INV_BUFFERS)
+			single_sent = MAX_WRITE_INV_BUFFERS;
+
+		/* put more handshake info to detect skew cases */
+		//req->begin =
+		//req->fence =
+		//req->end =
+		req->origin_ws = ws->id;
+
+		req->wr_cnt = single_sent;
+		req->iter = iter;
+		req->total_iter = total_iter;
+		/* can mean */
+
+		/* read from garry */
+		memcpy(req->addrs, rc->inv_addrs + sent_cnt,
+							sizeof(*req->addrs) * single_sent);
+
+		/* optimize - send_size = sizeof(*req) -
+		 *							sizeof(req->addrs) +
+		 *							(sizeof(*req->addrs) * single_sent)
+		 */
+		/* TODO: potential BUG. better to use pending */
+		printk("single_sent %d / sent_cnt %d / local_wr_cnt %d %d/%d ->\n",
+				single_sent, sent_cnt, local_wr_cnt, iter, total_iter);
+		pcn_kmsg_post(PCN_KMSG_TYPE_PAGE_MERGE_REQUEST, nid, req,
+								sizeof(*req) - sizeof(req->addrs) +
+								(sizeof(*req->addrs) * single_sent));
+								//sizeof(*req));
+
+		iter++;
+		sent_cnt += single_sent;
+
+		if (zero_case)
+			break;
+	} while (sent_cnt < local_wr_cnt);
+
+	/* optimization: these sends can be parallel */
 	/* send even no diff */
-    pcn_kmsg_post(PCN_KMSG_TYPE_PAGE_MERGE_REQUEST, nid, req, sizeof(*req));
+    //pcn_kmsg_post(PCN_KMSG_TYPE_PAGE_MERGE_REQUEST, nid, req, sizeof(*req));
 
-	__put_task_remote(rc);
-	wait_at_station(ws);
+	/* clean all for each run */
+	for (i = 0; i < local_wr_cnt; i++)
+		rc->inv_addrs[i] = 0;
+	local_wr_cnt = 0;
+
+	wait_at_station(ws); // change to consider iters
 }
 
 bool find_collision_inv(page_merge_request_t *req, page_merge_response_t *res)
@@ -73,11 +139,14 @@ bool find_collision_inv(page_merge_request_t *req, page_merge_response_t *res)
 	int scatters = 0;
 	struct task_struct *tsk = __get_task_struct(req->remote_pid);
 	struct remote_context *rc = get_task_remote(tsk);
+	BUG_ON(!rc || !tsk);
+
 
 	// TODO
 	// 1. no collision == inv(both)
 	// 2. collision = generate diffs + inv(if_remote) (need_merge=true)
 	//    				-> res->diffs = xxx;
+
 
 	// find out address first
 	// clear at this point
@@ -87,11 +156,17 @@ bool find_collision_inv(page_merge_request_t *req, page_merge_response_t *res)
 	return scatters;
 }
 
-/* -> Remote */
+/* -> [Remote] */
 static void process_page_merge_request(struct work_struct *work)
 {
 	START_KMSG_WORK(page_merge_request_t, req, work);
     page_merge_response_t *res = pcn_kmsg_get(sizeof(*res));
+
+	/* put more handshake info to detect skew cases */
+	//req->begin =
+	//req->fence =
+	//req->end =
+	// handshak remote status
 
 	res->scatters = find_collision_inv(req, res);
 
@@ -100,31 +175,43 @@ static void process_page_merge_request(struct work_struct *work)
 
 	res->origin_pid = req->origin_pid;
     res->origin_ws = req->origin_ws;
-    res->remote_pid = req->remote_pid;
+    //res->remote_pid = req->remote_pid;
+
 	/* not only 1 page man... 32k-4 8-1 = 7diffs......*/
 //	res->scatters = total;
 //	res->merge_id = iter_num;
+
+	res->wr_cnt = req->wr_cnt;
+	res->iter = req->iter;
+	res->total_iter = req->total_iter;
+
     pcn_kmsg_post(PCN_KMSG_TYPE_PAGE_MERGE_RESPONSE,
 				PCN_KMSG_FROM_NID(req), res, sizeof(*res));
     END_KMSG_WORK(req);
 }
 
-/* -> Local */
+/* -> back[Local] */
 static int handle_page_merge_response(struct pcn_kmsg_message *msg)
 {
     page_merge_response_t *res = (page_merge_response_t *)msg;
     struct wait_station *ws = wait_station(res->origin_ws);
 
-	if (!res->scatters) /* no need to merge */
-		goto done;
+//	if (!res->scatters) /* no need to merge */
+//		goto done;
+
+	//res->wr_cnt = req->wr_cnt;
+	//res->iter = req->iter;
+	//res->total_iter = req->total_iter;
 
 	/* TODO:
 	 * 1. fixup
 	 * 2. sync with remote
 	 */
 
-done:
-	complete(&ws->pendings);
+//done:
+	/* TODO: potential BUG. better to use pending */
+	if (res->iter == res->total_iter)
+		complete(&ws->pendings);
 
     pcn_kmsg_done(res);
     return 0;
@@ -134,7 +221,8 @@ done:
  * put rc and pirnk outsite !!!!!!!!!!1
  * mor general
  */
-static int __popcorn_barrier(struct remote_context *rc, int a)
+#if 0
+static bool __popcorn_barrier(struct remote_context *rc, int a)
 {
 	bool leader = false;
 	int left_t = atomic_dec_return(&rc->barrier);
@@ -144,16 +232,16 @@ static int __popcorn_barrier(struct remote_context *rc, int a)
 		leader = true;
 		//SYNCPRINTK("[*][%d] left_t %d\n", current->pid, left_t);
 
-		SYNCPRINTK("=== +[*%d] BARRIER %5s! line %d mb %lu b %lu f %lu t %d "
+		printk("=== +[*%d] BARRIER %5s! line %d mb %lu b %lu f %lu t %d "
 				"Hand Shake done. ===\n",
 				current->pid, "BEGIN", a, current->begin_m_cnt,
 				current->begin_cnt, current->tso_fence_cnt, rc->threads_cnt);
 
 		//__find_conflictions(!my_nid);
-		if (!my_nid)
-			__find_conflictions(1);
+		if (my_nid == 0)
+			__find_conflictions(1, rc);
 		else
-			__find_conflictions(0);
+			__find_conflictions(0, rc);
 
 		/* Race Consition !!!!! wait all are in the wq */
 		while (atomic_read(&rc->pendings) != rc->threads_cnt - 1)
@@ -189,7 +277,52 @@ static int __popcorn_barrier(struct remote_context *rc, int a)
 
 	return leader;
 }
+#endif
 
+static bool __popcorn_barrier_begin(struct remote_context *rc, int a)
+{
+	bool leader = false;
+	int left_t = atomic_dec_return(&rc->barrier);
+	BUG_ON(rc->threads_cnt > 96);
+
+	if (left_t == 0) {
+		leader = true;
+		printk("=== +[*%d] BARRIER %5s! line %d mb %lu b %lu f %lu t %d "
+				"Hand Shake done. ===\n",
+				current->pid, "BEGIN", a, current->begin_m_cnt,
+				current->begin_cnt, current->tso_fence_cnt, rc->threads_cnt);
+	}
+
+	return leader;
+}
+
+static void __popcorn_barrier_end(struct remote_context *rc, int a, bool leader)
+{
+	if (leader) {
+		printk("=== -[*%d] BARRIER %5s! line %d mb %lu b %lu f %lu t %d "
+				"Hand Shake done. ===\n\n",
+				current->pid, "END", a, current->begin_m_cnt,
+				current->begin_cnt, current->tso_fence_cnt, rc->threads_cnt);
+
+		/* Race Consition !!!!! wait all are in the wq */
+		while (atomic_read(&rc->pendings) != rc->threads_cnt - 1)
+			io_schedule(); // seems important to yield the atomic op bus in VM
+
+		atomic_inc(&rc->pendings);
+		atomic_set(&rc->barrier, rc->threads_cnt);
+		wake_up_all(&rc->waits);
+		atomic_dec(&rc->pendings);
+		//SYNCPRINTK("[*][%d] done\n", current->pid);
+	} else {
+		DEFINE_WAIT(wait);
+		//SYNCPRINTK("[ ][%d] left_t %d\n", current->pid, left_t);
+        atomic_inc(&rc->pendings);
+		prepare_to_wait_exclusive(&rc->waits, &wait, TASK_UNINTERRUPTIBLE);
+		io_schedule();
+		finish_wait(&rc->waits, &wait);
+		atomic_dec(&rc->pendings);
+	}
+}
 
 void collect_tso_wr(void)
 {
@@ -223,14 +356,14 @@ static int __popcorn_tso_fence(int a, void __user * b)
 {
 	int i;
 	bool leader = false;
-	unsigned long tmp; // TODO: change name
+	unsigned long inv_cnt; // TODO: change name
 	struct remote_context *rc = get_task_remote(current);
 	SYNCPRINTK("\t(maybe implicit) [%d] %s():\n", current->pid, __func__);
 
 	if (!current->tso_region) {
 		//PCNPRINTK_ERR("[%d] BUG tso_region order violation when \"unlock\"\n",
 		//														current->pid);
-		//goto out;
+		goto out;
 	}
 
 	if (!current->tso_wr_cnt) {
@@ -239,7 +372,19 @@ static int __popcorn_tso_fence(int a, void __user * b)
 		current->tso_nobenefit_region_cnt++;
 	}
 
-	__popcorn_barrier(rc, a);
+#if POPCORN_BARRIER
+	//leader = __popcorn_barrier(rc, a);
+	leader = __popcorn_barrier_begin(rc, a);
+
+	if (leader) {
+		if (my_nid == 0)
+			__find_conflictions(1, rc);
+		else
+			__find_conflictions(0, rc);
+	}
+
+	__popcorn_barrier_end(rc, a, leader);
+#endif
 
 	current->tso_fence_cnt++;
 
@@ -249,15 +394,17 @@ static int __popcorn_tso_fence(int a, void __user * b)
 	//					current->accu_tso_wr_cnt);
 
 	/* check buffer and invalidate all */
-	if (current->tso_wr_cnt > MAX_WRITE_INV_BUFFERS)
-		tmp = MAX_WRITE_INV_BUFFERS; /* suppresed performance */
-	else
-		tmp = current->tso_wr_cnt;
+	if (current->tso_wr_cnt > MAX_WRITE_INV_BUFFERS) {
+		BUG(); /* should stop in runtime, not here */
+		//inv_cnt = MAX_WRITE_INV_BUFFERS; /* suppresed performance */
+	} else {
+		inv_cnt = current->tso_wr_cnt;
+	}
 
 	if (!batch) {
 		BUG_ON("Not support: wanna have clean code base");
 #if 0
-		for (i = 0; i < tmp; i++) { /* deterministic loop */
+		for (i = 0; i < inv_cnt; i++) { /* deterministic loop */
 			unsigned long addr = current->buffer_inv_addrs[i];
 			int peers;
 			unsigned long *pi;
@@ -266,7 +413,7 @@ static int __popcorn_tso_fence(int a, void __user * b)
 			int except_nid = my_nid;
 
 			// send inv (addr)
-			if (!my_nid) { /* 1. first deal w/ origin only*/
+			if (my_nid == 0) { /* 1. first deal w/ origin only*/
 				int nid;
 				struct wait_station *ws = get_wait_station(current);
 				SYNCPRINTK("[%d] fixing %lx \n", current->pid, addr);
@@ -321,27 +468,31 @@ static int __popcorn_tso_fence(int a, void __user * b)
 		}
 #endif
 	} else { // batch
-		if (tmp) {
+		if (inv_cnt) {
 			// 2. batch: PCN_KMSG_TYPE_PAGE_INVALIDATE_BATCH_REQUEST
 			int nid = -1, j;
-			unsigned long *addrs = current->buffer_inv_addrs;
-#ifdef CONFIG_POPCORN_CHECK_SANITY
+#if INVOKE_INV
+			unsigned long *addrs;
+#endif
+
 			if (my_nid == 0)
 				nid = 1;
 			else if (my_nid == 1)
 				nid = 0;
-#endif
+#if INVOKE_INV
+			addrs = current->buffer_inv_addrs;
 			SYNCPRINTK("[%d] revoking->@%d addrs[0]=%lx tso_wr_cnt %llu\n",
 							current->pid, nid, addrs[0], current->tso_wr_cnt);
 
 			// lock? befor calling this function? check claim_
-			__revoke_page_ownerships(current, nid, /* 2-node assumption */
-									rc->remote_tgids[nid], addrs, tmp);
+			remote_revoke_page_ownerships(current, nid, /* 2-node assumption */
+									rc->remote_tgids[nid], addrs, inv_cnt);
 
 			SYNCPRINTK("[%d] revoking done  addrs[0]=%lx tso_wr_cnt %llu\n",
 								current->pid, addrs[0], current->tso_wr_cnt);
-
-			for (j = 0; j < tmp; j++)
+#endif
+			/* important: makke sure the location */
+			for (j = 0; j < inv_cnt; j++)
 				current->buffer_inv_addrs[i] = 0;
 		}
 	}
@@ -442,9 +593,9 @@ SYSCALL_DEFINE2(popcorn_tso_begin_manual, int, a, void __user *, b)
 			print = true;
 #endif
 			PCNPRINTK_ERR("[%d] BUG tso_region order violation when \"lock\" "
-						"region (%s) tso_wr %llu tso_wx %llu line %d\n",
-									current->pid, current->tso_region?"O":"X",
-									current->tso_wr_cnt, current->tso_wx_cnt, a);
+							"region (%s) tso_wr %llu tso_wx %llu line %d\n",
+							current->pid, current->tso_region?"O":"X",
+							current->tso_wr_cnt, current->tso_wx_cnt, a);
 		}
 		violation++;
 		//__popcorn_tso_fence(a, b); /* weired case..... but we have to fix NMW */
@@ -528,6 +679,20 @@ int __init popcorn_sync_init(void)
             PCN_KMSG_TYPE_PAGE_MERGE_REQUEST, page_merge_request);
     REGISTER_KMSG_HANDLER(
             PCN_KMSG_TYPE_PAGE_MERGE_RESPONSE, page_merge_response);
+
+	/* dbg */
+	printk("8* %d * %lu = [%lu]/ PAGE = [%lu] pgs\n",
+			MAX_ALIVE_THREADS, MAX_WRITE_INV_BUFFERS,
+			8 * MAX_ALIVE_THREADS * MAX_WRITE_INV_BUFFERS,
+			8 * MAX_ALIVE_THREADS * MAX_WRITE_INV_BUFFERS / PAGE_SIZE);
+
+	printk("msg: available size %lu = available pages %lu / %lu bytes\n"
+			" wrong #[%lu]\n",
+			PCN_KMSG_MAX_PAYLOAD_SIZE,
+			(PCN_KMSG_MAX_PAYLOAD_SIZE / PAGE_SIZE),
+			(PCN_KMSG_MAX_PAYLOAD_SIZE / PAGE_SIZE) * PAGE_SIZE,
+			(((PCN_KMSG_MAX_PAYLOAD_SIZE / PAGE_SIZE) * PAGE_SIZE) /
+												sizeof(unsigned long)));
 
 	return 0;
 }
