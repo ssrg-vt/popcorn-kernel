4155fe71bb1c4a8b7ffbbb60a70a9fef7ec2a7d6
bx <bx@narfindustries.com>
2018-12-18 14:44:37 -0500

    remove PPC from popcorn's pgtable.h

--- kernel/popcorn/pgtable.h
+++ kernel/popcorn/pgtable.h
@@ -47,29 +47,6 @@ static inline bool pte_is_present(pte_t entry)
 {
 	return (pte_val(entry) & PTE_VALID) == PTE_VALID;
 }
-#elif defined(CONFIG_PPC64)
-static inline unsigned long pte_flags(pte_t entry)
-{
-	return (unsigned long)entry.pte;
-}
-
-static inline pte_t pte_make_invalid(pte_t entry)
-{
-	entry.pte &= ~_PAGE_PRESENT;
-
-	return entry;
-}
-
-static inline pte_t pte_make_valid(pte_t entry)
-{
-	entry.pte |= _PAGE_PRESENT;
-
-	return entry;
-}
-static inline bool pte_is_present(pte_t entry)
-{
-	return (pte_val(entry) & _PAGE_PRESENT) == _PAGE_PRESENT;
-}
 #else
 #error "The architecture is not supported yet."
 #endif
