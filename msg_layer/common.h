da36ec74e8da6b5d446f07dbf8647b59090d7a19
hemanthr28 <hemanthr@vt.edu>
2023-03-01 22:44:41 -0500

    Updated popcorn files-5

--- msg_layer/common.h
+++ msg_layer/common.h
@@ -11,23 +11,17 @@
 #include <popcorn/pcn_kmsg.h>
 #include <popcorn/bundle.h>
 #include <popcorn/debug.h>
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/kernel.h>
+
 #include <linux/inet.h>
 #include <linux/inetdevice.h>
 #include <linux/netdevice.h>
 
-#include "config.h"
+#define MAX_NUM_NODES	32
 
-#define MAX_NUM_NODES		ARRAY_SIZE(ip_addresses)
+
 static uint32_t ip_table[MAX_NUM_NODES] = { 0 };
 static uint32_t max_nodes = MAX_NUM_NODES;
 
-
-static char *ip = "N";
-module_param(ip,charp, 0000);
-MODULE_PARM_DESC(ip, "");
 
 static uint32_t __init __get_host_ip(void)
 {
@@ -38,7 +32,7 @@ static uint32_t __init __get_host_ip(void)
 		for (ifaddr = d->ip_ptr->ifa_list; ifaddr; ifaddr = ifaddr->ifa_next) {
 			int i;
 			uint32_t addr = ifaddr->ifa_local;
-			for (i = 0; i < MAX_NUM_NODES; i++) {
+			for (i = 0; i < max_nodes; i++) {
 				if (addr == ip_table[i]) {
 					return addr;
 				}
@@ -52,46 +46,12 @@ bool __init identify_myself(void)
 {
 	int i;
 	uint32_t my_ip;
-	printk("%s\n",ip);
-	if(ip[0]=='N'){
-		PCNPRINTK("Loading default node configuration...\n");
 
-		for (i = 0; i < MAX_NUM_NODES; i++) {
-			ip_table[i] = in_aton(ip_addresses[i]);
-		}
-	}
-	else{
-		PCNPRINTK("Loading user configuration...\n");
-		int j, k = 0;
-		char* tem, *temp;
-/*
-		for (i = 0; i < MAX_NUM_NODES; i++) {
-			tem = (char*)kmalloc(15*sizeof(char),GFP_KERNEL);
-			for(j = 0; j< 16; j++) {
-				if( k == strlen(ip)) {i==MAX_NUM_NODES; break;}
-				if (ip[k]==':'){ k++;break;} else {tem[j] = ip[k]; k++;}
-			}
-			
-			printk("tem[%d] %s\n",i,tem);
-			ip_table[i] = in_aton(tem);
-		}
-		for (i = 0; i < MAX_NUM_NODES; i++) {
-                        printk("%zu\n",ip_table[i]);
-                }
-*/
-		temp=strlen(ip) + ip;
-		while (tem = strchrnul(ip, ',')) {
-			*tem = 0;
-			ip_table[k++] = in_aton(ip);
-			ip=tem+1;
-			if (ip > temp)
-				break;
-		}
-	}
+	PCNPRINTK("Loading node configuration...");
 
 	my_ip = __get_host_ip();
 
-	for (i = 0; i < MAX_NUM_NODES; i++) {
+	for (i = 0; i < max_nodes; i++) {
 		char *me = " ";
 		if (my_ip == ip_table[i]) {
 			my_nid = i;
@@ -101,6 +61,7 @@ bool __init identify_myself(void)
 	}
 
 	if (my_nid < 0) {
+		PCNPRINTK_ERR("%d", my_nid);
 		PCNPRINTK_ERR("My IP is not listed in the node configuration\n");
 		return false;
 	}
