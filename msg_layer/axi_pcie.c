56b2b5eb6b87fa9c55670a380b341f1c04411afa
hemanthr28 <hemanthr@vt.edu>
2023-03-14 14:44:28 -0400

    Updated driver code11

--- msg_layer/axi_pcie.c
+++ msg_layer/axi_pcie.c
@@ -557,8 +557,8 @@ static int axidma_remove(struct platform_device *pdev)
 
 static const struct of_device_id axidma_compatible_of_ids[] = {
       { .compatible = "simple-bus"},
-    //{ .compatible = "xlnx,pcie-us-rqrc-1.0" },
-    //{ .compatible = "xlnx,protocol-processor-v1-0-1.0" },
+      { .compatible = "xlnx,pcie-us-rqrc-1.0" },
+      { .compatible = "xlnx,protocol-processor-v1-0-1.0" },
     {}
 };
 
