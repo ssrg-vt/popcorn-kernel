89cfe3c4d7ad79d09f74ff2995f32ac4070837f3
hemanthr28 <hemanthr@vt.edu>
2023-03-16 12:48:31 -0400

    Kernel updates13

--- msg_layer/axi_pcie.c
+++ msg_layer/axi_pcie.c
@@ -621,7 +621,7 @@ int pcie_axi_kmsg_send(int nid, struct pcn_kmsg_message *msg, size_t size)//0,
     */
     for(i=0; i<FDSM_MSG_SIZE/8; i++){ //send 8KB data, 8B in each transfer
             printk("copying data %d\n", i);
-            printk("Data %d = llx\n", i, *(dma_addr_pntr+i));
+            printk("Data %d = %llx\n", i, *(dma_addr_pntr+i));
             //writeq(cpu_to_le64(*(volatile __le64*)(mapped_addr+i)), x86_host_addr + i);
             writeq(*(dma_addr_pntr+i), (x86_host_addr + i));
 
