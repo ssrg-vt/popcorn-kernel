3656078492391b3cc88068c055ecf06c2dd525c9
hemanthr28 <hemanthr@vt.edu>
2023-03-22 09:16:58 -0400

    Debugging nid9

--- msg_layer/axi_pcie.c
+++ msg_layer/axi_pcie.c
@@ -815,7 +815,7 @@ static int __init axidma_init(void)
     printk("Readq = %llx\n",readq(prot_proc_addr));
     */
 
-    my_nid = 1;
+    my_nid = 0;
     //Write the node ID to the protocol processor
     //iowrite32(0x1, prot_proc_addr+0x34);//Enable when fDSM is enabled
     //printk("prot_proc_addr=%p\n",ioread32(prot_proc_addr+0x34));
