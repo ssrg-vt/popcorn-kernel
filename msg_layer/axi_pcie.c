8f2974dcf636b5f3fade39fa8ed585097595f09a
hemanthr28 <hemanthr@vt.edu>
2023-03-23 01:16:59 -0400

    Updated driver code1

--- msg_layer/axi_pcie.c
+++ msg_layer/axi_pcie.c
@@ -338,12 +338,12 @@ static int poll_dma(void* arg0)
     int recv_index = 0, index = 0, tmp = 0;
 
     tmp = __get_recv_index(recv_queue);
-    //printk("In poll, the addr is %llx\n", virt_to_phys((recv_queue->work_list[tmp]->addr)));
+    printk("In poll, the addr is %llx\n", virt_to_phys((recv_queue->work_list[tmp]->addr)));
     while (!kthread_freezable_should_stop(&was_frozen)) {
         //printk("polling...");
         //c2h_desc_complete = counter_rx; //poll_c2h_wb->completed_desc_count;
         //h2c_desc_complete = counter_tx; //poll_h2c_wb->completed_desc_count;
-        printk("Data found in poll = %llx\n", *(uint64_t *)((recv_queue->work_list[tmp]->addr)+(1023*8)));
+        //printk("Data found in poll = %llx\n", *(uint64_t *)((recv_queue->work_list[tmp]->addr)+(1023*8)));
         if (*(uint64_t *)((recv_queue->work_list[tmp]->addr)+(1023*8)) == 0xd010d010) { //possible performance improvement here!
             printk("In IF\n");
             //write_register(0x00, (u32 *)(xdma_c + c2h_ctl));
