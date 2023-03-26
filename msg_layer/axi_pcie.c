ec8cd7006290c365fcb9d010e0bec4e5fbed7319
hemanthr28 <hemanthr@vt.edu>
2023-03-26 15:45:59 -0400

    Updates14

--- msg_layer/axi_pcie.c
+++ msg_layer/axi_pcie.c
@@ -59,7 +59,7 @@ static unsigned long *c2h_poll_addr;
 static unsigned long *h2c_poll_addr;
 static dma_addr_t c2h_poll_bus;
 static dma_addr_t h2c_poll_bus;
-static void *base_addr; 
+static void volatile *base_addr; 
 static dma_addr_t base_dma;
 
 static struct iommu_domain *domain; 
@@ -557,7 +557,7 @@ out:
 //Rewrite this function and the poll handles -> poll_dma
 static int __start_poll(void)
 {   
-    poll_tsk = kthread_run(&poll_dma, NULL, "Poll_Handler");
+    poll_tsk = kthread_run(poll_dma, NULL, "Poll_Handler");
     if (IS_ERR(poll_tsk)) {
         PCNPRINTK("Error Instantiating Polling Handler\n");
         return 1;
@@ -762,8 +762,8 @@ static void __exit axidma_exit(void)
 
     set_popcorn_node_online(nid, false);
 
-    dma_free_coherent(&pdev->dev, SZ_2M, base_addr, base_dma);
-    //kfree(base_addr);
+    //dma_free_coherent(&pdev->dev, SZ_2M, base_addr, base_dma);
+    kfree(base_addr);
     /*
     while (send_work_pool) {
         struct send_work *work = send_work_pool;
@@ -855,27 +855,27 @@ static int __init axidma_init(void)
     pdev = of_find_device_by_node(x86_host);
     //ret = pci_set_dma_mask(pdev, DMA_BIT_MASK(32));
     
-    
+    /*
     dma_set_mask_and_coherent(&pdev->dev, DMA_BIT_MASK(32));
     base_addr = dma_alloc_coherent(&pdev->dev, SZ_2M, &base_dma, GFP_KERNEL);//2 x 64 regions x 8KB
+    */
     
-    /*
     base_addr = kzalloc(SZ_2M, GFP_KERNEL);
     if(!base_addr){
         goto out_free;
     }
-    */
+    
     //printk("base_addr=%llx\n",base_addr);
     //printk("base_dma=%llx\n",base_dma);//This address cannot be used without a DMA engine. 
     //printk("&base_dma=%llx\n",&base_dma);
 
-//#ifdef CONFIG_ARM64 
+/*#ifdef CONFIG_ARM64 
         domain = iommu_get_domain_for_dev(&pdev->dev);
         if (!domain) goto out_free;
     
         ret = iommu_map(domain, base_dma, virt_to_phys(base_addr), SZ_2M, IOMMU_READ | IOMMU_WRITE);
         if (ret) goto out_free;
-//#endif
+//#endif*/
   
     /*
     if (__setup_ring_buffer())
