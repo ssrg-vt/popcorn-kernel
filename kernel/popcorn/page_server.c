33f82074f19a22adbfc0186e14624b5c9e5cd95d
hemanthr28 <hemanthr@vt.edu>
2023-04-18 14:03:10 -0400

    nid issue

--- kernel/popcorn/page_server.c
+++ kernel/popcorn/page_server.c
@@ -2483,7 +2483,7 @@ static int __pcie_axi_handle_lcfault_at_remote(struct vm_fault *vmf)
 	printk("Before prot proc handler\n");
 	printk("from_nid=%d\n", tsk->origin_nid);
 	prot_proc_handle_localfault((unsigned long)vmf, addr, (unsigned long)instruction_pointer(current_pt_regs()), pkey,
-	tsk->pid, tsk->origin_pid, tsk->origin_nid, vmf->flags, ws->id, 1);
+	tsk->pid, tsk->origin_pid, 1, vmf->flags, ws->id, 1);//replaced tsk->origin_nid with 1
 	printk("Before wait station\n");
 	rp = wait_at_station(ws);
 	printk("After wait station\n");
